567b8a15156c9f5a5b6620aab8866a9a14b94d28
Sujit Kumar Das <sanjib121kumardas@gmail.com>
2023-05-26 18:43:52 +0530

    Second Commit

--- app/src/main/java/com/example/android/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/android/instagramclone/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.android.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+//import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Tag", "OnCreate Started");
+
+
     }
+
+    private void setupBottomNavigation()
+    {
+        Log.d("BottomNavigation", "BottomNavigationSetup");
+        BottomNavigationView bottomNavigationViewEx = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+
+    }
+
 }
